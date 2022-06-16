68b7244c42bf4cf7f48bc39568f2f4d71bf355a0
Charly <carloscharlywilliams@gmail.com>
2022-06-15 21:41:44 -0300

    Nuevas Carpetas

---
+++ backend/modelos/modelodeproductos.php
@@ -0,0 +1,21 @@
+<!-- 
+El Modelo de producto debe tener por lo menos estos atributos:
+
+Nombre
+Descripción
+Precio
+Stock
+Pertenecer a una o más categorías.
+Tener por lo menos una imagen. -->
+<?php
+/**
+ * TRAER TODOS LOS PRODUCTOS DE LA BASE DE DATOS
+ * int $id
+ */
+include 'conexion/php';
+function get_producto($id)
+{
+    $query = "SELECT * FROM productos WHERE id=$id";
+    return conectame()->$query;
+}
+get_producto(2);
--- modelos/modelodeproductos.php
+++
@@ -1,9 +0,0 @@
-<!-- 
-El Modelo de producto debe tener por lo menos estos atributos:
-
-Nombre
-Descripción
-Precio
-Stock
-Pertenecer a una o más categorías.
-Tener por lo menos una imagen. -->
