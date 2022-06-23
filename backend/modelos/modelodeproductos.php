342dba4cc4b290ecabeb5ac73e0e42869baa176d
Charly <carloscharlywilliams@gmail.com>
2022-06-22 21:26:56 -0300

    update producto

--- backend/modelos/modelodeproductos.php
+++ backend/modelos/modelodeproductos.php
@@ -24,7 +24,11 @@ function get_producto($id)
     return $conexion->query($query)->fetch_all(MYSQLI_ASSOC);
 }
 
-function insert_producto($id)
+/**
+ * INSERTA UN PRODUCTO EN LA BASE DE DATOS
+ */
+
+function insert_producto()
 {
     $nombre_producto = $_POST['nombre_producto'];
     $descripcion_producto = $_POST['descripcion_producto'];
@@ -36,23 +40,51 @@ function insert_producto($id)
 
     $query = conectame()->query("INSERT INTO productos(nombre, descripcion, precio, stock, imagen)VALUES ('$nombre_producto', '$descripcion_producto', '$precio_producto', '$stock_producto', '$imagen_producto')");
 
-    /* una vez que inserta los datos recibidos, obtiene el id del producto y lo inserta en la tabla categoriasprocucto para que coincidan unos con otros */ 
- 
-    /*llama a el id del producto insertado*/ 
-    $consulta1 = "SELECT id FROM productos order by 1 desc LIMIT 1"; 
+    /* una vez que inserta los datos recibidos, obtiene el id del producto y lo inserta en la tabla categoriasprocucto para que coincidan unos con otros */
+
+    /*llama a el id del producto insertado*/
+    $consulta1 = "SELECT id FROM productos order by 1 desc LIMIT 1";
     $resultado1 = mysqli_query(conectame(), $consulta1);
-    if (mysqli_num_rows($resultado1) == 1) { 
- 
-        $row = mysqli_fetch_array($resultado1); 
-        $id_producto = $row['id']; 
-    } 
- 
+    if (mysqli_num_rows($resultado1) == 1) {
+
+        $row = mysqli_fetch_array($resultado1);
+        $id_producto = $row['id'];
+    }
+
     //le doy un id_producto a la tabla
     $query5 = conectame()->query("INSERT INTO categoriasproductos(id_producto)VALUES('$id_producto')");
-    /*agrega id_categoria a el nuevo producto*/ 
-    $query6 = conectame()->query("UPDATE categoriasproductos SET id_categoria = $categoria_producto WHERE id_producto = $id_producto"); 
-
+    /*agrega id_categoria a el nuevo producto*/
+    $query6 = conectame()->query("UPDATE categoriasproductos SET id_categoria = $categoria_producto WHERE id_producto = $id_producto");
 }
 
 
+function update_producto()
+{
 
+    $id_producto = $_GET['id'];
+    $nombre_producto = $_POST['nombre_producto'];
+    $descripcion_producto = $_POST['descripcion_producto'];
+    $precio_producto = $_POST['precio_producto'];
+    $stock_producto = $_POST['stock_producto'];
+    $imagen_producto = $_POST['imagen_producto'];
+    $categoria_producto = $_POST['categoria_producto'];
+
+    $query = conectame()->query("UPDATE productos SET nombre='$nombre_producto', descripcion='$descripcion_producto', precio ='$precio_producto', stock_producto='$stock_producto',imagen='$imagen_producto' WHERE id=$id_producto");
+
+    $query2 = conectame()->query("UPDATE categoriasproductos SET id_categoria = $categoria_producto WHERE id_producto = $id_producto");
+    /* una vez que inserta los datos recibidos, obtiene el id del producto y lo inserta en la tabla categoriasprocucto para que coincidan unos con otros */
+
+    /*llama a el id del producto insertado*/
+    $consulta1 = "SELECT id FROM productos order by 1 desc LIMIT 1";
+    $resultado1 = mysqli_query(conectame(), $consulta1);
+    if (mysqli_num_rows($resultado1) == 1) {
+
+        $row = mysqli_fetch_array($resultado1);
+        $id_producto = $row['id'];
+    }
+
+    //le doy un id_producto a la tabla
+    $query5 = conectame()->query("INSERT INTO categoriasproductos(id_producto)VALUES('$id_producto')");
+    /*agrega id_categoria a el nuevo producto*/
+    $query6 = conectame()->query("UPDATE categoriasproductos SET id_categoria = $categoria_producto WHERE id_producto = $id_producto");
+}
